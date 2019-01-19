491f5fb13dcd6f0b813ceeb80bce8faee27c9098
Cecilia Thulin <cecilia.thulin@gmail.com>
2019-01-19 12:30:25 +0100

    fixed allow hosts settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e98elqj^5t6hx8c&8wp3s*31tw$+2xgt4ouiik1^#99n8do3-i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ' .pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
